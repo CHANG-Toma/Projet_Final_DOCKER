98353479edd63c85886a7872d22b3cc669fb2f4d
Toma Chang <toma11chang@gmail.com>
2024-01-03 12:08:32 +0100

    fix : db (login and register on each nginx work successfully)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 # Attend que la base de données soit prête
 echo "En attente de la base de données..."
-sleep 10
+sleep 5
 
 # Exécute les commandes Laravel si le fichier artisan existe
 if [ -f artisan ]; then
     php artisan key:generate
-    php artisan migrate:fresh --seed
+    php artisan migrate --seed  # Modifié pour ne pas supprimer les tables existantes
 else
     echo "Pas de fichier artisan"
 fi
